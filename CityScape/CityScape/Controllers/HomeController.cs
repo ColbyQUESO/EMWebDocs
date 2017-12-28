26ab778e31aa482aa885e461fd48745a671f9c82
Colby Curtis (Desktop) <colbyc@mviusa.com>
2017-12-28 10:06:05 -0700

    Initial commit CityScape

---
+++ CityScape/CityScape/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CityScape.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "CityScape is a cool software that makes city documents available to the general public.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
